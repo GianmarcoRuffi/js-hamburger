7f15791c0a40126ab3c57102f803f165503cd769
GianmarcoRuffi <gianmarco.ruffi@outlook.it>
2022-03-25 17:13:13 +0100

    cancellati precedenti comandi

---
+++ js/main.js
@@ -0,0 +1,11 @@
+/*Hamburger menu:
+mostrare / nascondere il menu principale
+(ricordate che per vedere l’hamburger in alto a destra dovete scendere a risoluzioni tipo tablet o mobile). */
+
+let menuOn = document.querySelector(".fas.fa-bars");
+
+let hamburgerMenu = document.querySelector(".hamburger-menu");
+
+let menuClose = document.querySelector(".fas.fa-times");
+
+console.log(menuOn, hamburgerMenu, menuClose);
